06a799c6f92d2a11938dc17f88ac4703e7fc71df
duzhixiong <2908006163@qq.com>
2022-11-04 15:19:37 +0800

    20221104

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,38 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<div id="app">
+  <VirtualList :listData="data" :itemSize="120"/>
+</div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import VirtualList from "./components/VirtualList";
+let d = [];
+for (let i = 0; i < 1000; i++) {
+  d.push({ id: i, value: i });
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  name: "App",
+  data() {
+    return {
+      data: d
+    };
+  },
+  components: {
+    VirtualList
   }
+};
+</script>
+
+<style>
+html{
+  height: 100%;
+}
+body{
+  height: 100%;
+  margin:0;
+}
+#app{
+  height:100%;
 }
 </style>
